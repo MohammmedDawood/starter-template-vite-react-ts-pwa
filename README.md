8a176aa94eea995fa40f724814868574e02b9421
Mohammed Dawood <mahmedtony73@gmail.com>
2023-10-28 23:28:28 +0200

    feat(SW): edit package and vite config

--- README.md
+++ README.md
@@ -25,3 +25,29 @@ If you are developing a production application, we recommend updating the config
 - Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+
+## Features
+
+- [x] React 18
+- [x] Based on vite
+- [x] CSS in JS
+- [x] Typescript
+- [x] Mantine UI libarary
+- [x] Redux
+- [x] Eslint, prettier
+- [x] husky, lint-staged
+- [x] Configurable routes
+- [x] Lazy route
+- [x] Routes based nav
+- [ ] Cached router
+- [x] PWA
+- [ ] i18n
+- [x] RTL
+- [ ] Responsive
+- [ ] Animation
+- [ ] Themes
+- [x] Dark Mode
+
+# License
+
+MIT
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import manifest from './manifest.json';
 
+function handleBooleanEnv(env: string) {
+  return env === 'false' ? false : env === 'true' ? true : undefined;
+}
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -35,7 +38,9 @@ export default defineConfig(({ command, mode }) => {
 
       server: {
         // env.PORT is injected by loadEnv()
-        port: parseInt(env.PORT) || 3000,
+        // port: parseInt(env.PORT) || 3000,
+        port: +env.VITE_PORT,
+        open: handleBooleanEnv(env.VITE_OPEN),
       },
       resolve: {
         alias: {
@@ -70,11 +75,6 @@ export default defineConfig(({ command, mode }) => {
         }),
       ],
       // ...some configs
-
-      server: {
-        // env.PORT is injected by loadEnv()
-        port: parseInt(env.PORT) || 3000,
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, './src'),
@@ -82,6 +82,12 @@ export default defineConfig(({ command, mode }) => {
       },
       build: {
         outDir: './build',
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
       },
     };
   }
